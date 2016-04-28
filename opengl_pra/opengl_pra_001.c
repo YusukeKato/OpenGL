fab687ac6c32693f78f9b4416c675fca37380d13
YusukeKato <mikan1212.lion2a4p8c3@docomo.ne.jp>
2016-04-28 23:32:42 +0900

    opengl_pra_001.c
    2016.4.28
    goal : create 3D_GAME

---
+++ opengl_pra/opengl_pra_001.c
@@ -0,0 +1,37 @@
+/* 2016.4.28 */
+/* OpenGL */
+/* C */
+
+/* 目標：OpenGLで3Dゲームを作る（おもしろいやつ） */
+
+# include <stdio.h>
+# include <GL/glut.h>
+
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3d(1.0, 0.0, 0.0);
+	glBegin(GL_LINE_LOOP);
+	glVertex2d(-0.5, -0.5);
+	glVertex2d(0.5, -0.5);
+	glVertex2d(0.5, 0.5);
+	glVertex2d(-0.5, 0.5);
+	glEnd();
+	glFlush();
+}
+
+void init(void)
+{
+	glClearColor(0.0, 1.0, 1.0, 1.0);
+}
+
+int main(int argc, char *argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA);
+	glutCreateWindow(argv[0]);
+	glutDisplayFunc(display);
+	init();
+	glutMainLoop();
+	return 0;
+}
